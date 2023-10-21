29f408be87d89d79f35732c07f96f7db0cd730f3
Ren Yoshida <ren.yoshida1992@gmail.com>
2023-10-22 01:28:07 +0900

    update

--- nvim/lua/functions.lua
+++ nvim/lua/functions.lua
@@ -7,26 +7,3 @@ function GetGitRoot()
 		return vim.fn.expand("%:p:h")
 	end
 end
-
--- git管理されていれば:GFiles、そうでなければ:Filesを実行する
-function SearchFzf()
-	local is_git = vim.fn.system("git status")
-	if vim.v.shell_error ~= 0 then
-		vim.cmd("Files")
-	else
-		vim.cmd("GFiles")
-	end
-end
-
--- ripgrepを使ってプロジェクト内のファイルを検索する
-function FZGrep(query, fullscreen)
-	local git_root = GetGitRoot()
-	local command_fmt = "rg --column --line-number --no-heading --color=always --smart-case -- %s "
-		.. git_root
-		.. " || true"
-	local initial_command = string.format(command_fmt, vim.fn.shellescape(query))
-	local reload_command = string.format(command_fmt, "{q}")
-	local spec = { options = { "--phony", "--query", query, "--bind", "change:reload:" .. reload_command } }
-	vim.fn["fzf#vim#grep"](initial_command, 1, vim.fn["fzf#vim#with_preview"](spec), fullscreen)
-end
-
